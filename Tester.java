1500909ee8f4f5177df3e950f32d04c149a7b96f
LM0177733 <L00177733@atu.ie>
2025-11-24 23:57:27 +0000

    Add files via upload

---
+++ Tester.java
@@ -0,0 +1,44 @@
+public class Tester {
+
+	public static void main(String[] args) {
+        int rows=4;
+         int cols=5;
+		int[][] visited={{1,2,3,4},{1,2,3,4,5}};
+       for(int j=0;visited.length;j++)
+       {
+        if backtrack(start.rows, start.col)
+{
+       return backtrack();  // valid route found
+}
+   else
+{
+       return false; // no valid route
+}
+}
+       bool backtrack(int rowint,int colsint)
+       {
+       visited=new[row,col]//give it the same length and width as fabric
+       for(int i=0;visited.x(row).length;i++)//
+       {
+        for(int j=0;visited.y(col).length;j++)
+       {
+        if(row[i]!=null&&col[i]!=null)
+       {
+       if(row[i]!<101||col[i]!<101)
+       {
+       if(row[i]!>130||col[i]!<130)
+       {
+       return false;
+       }
+       }
+       }
+       }
+       }
+       return true;
+     
+       
+       }
+
+	}
+
+
