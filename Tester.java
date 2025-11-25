4b4f5dac9c7d4d73865ea9ef8d9219d1c16699af
LM0177733 <L00177733@atu.ie>
2025-11-25 01:39:41 +0000

    Add files via upload

--- Tester.java
+++ Tester.java
@@ -1,43 +1,78 @@
-public class Tester {
+import java.util.Scanner;
 
+public class Tester {
+int[][] visited={{1,2,3,4},{1,2,3,4,5}};
+ 
 	public static void main(String[] args) {
-        int rows=4;
-         int cols=5;
-		int[][] visited={{1,2,3,4},{1,2,3,4,5}};
-       for(int j=0;visited.length;j++)
+Scanner myObj = new Scanner(System.in);
+Tester newobj=new Tester();
+ System.out.println("Type the number of rows & Columns you want in the array");
+System.out.println("In the format of number of ROWS first then number of COLUMNS");
+ newobj.backtrack(1,5);
+
+    //   for(int j=0;j<visited.length;j++)
+    //   {
+     //   for(int i=0;i<visited.length;i++)
+    //   {
+     //   if(backtrack(visited[i], visited[j]))
+//{
+     //  backtrack.true;  // valid route found
+//}
+   //else
+//{
+      // return false; // no valid route
+//}
+    }
+
+       public boolean backtrack(int rowint,int colint)
        {
-        if backtrack(start.rows, start.col)
-{
-       return backtrack();  // valid route found
-}
-   else
-{
-       return false; // no valid route
-}
-}
-       bool backtrack(int rowint,int colsint)
+       
+        Scanner myObj = new Scanner(System.in);
+         for(int i=0;i<visited.length-1;i++)//
        {
-       visited=new[row,col]//give it the same length and width as fabric
-       for(int i=0;visited.x(row).length;i++)//
+        for(int j=0;j<visited[rowint].length-1;j++)
        {
-        for(int j=0;visited.y(col).length;j++)
+        System.out.println("Enter value");
+        visited[i][j]=myObj.nextInt();
+       }
+    }
+      for(int i=0;i<visited.length;i++)//
        {
-        if(row[i]!=null&&col[i]!=null)
+          for(int j=0;j<visited[i].length;j++)
        {
-       if(row[i]!<101||col[i]!<101)
+    System.out.println("Value is "+ visited[i][j]);
+       }
+      }
+             //int rows=4;
+        // int cols=5;
+		
+      // visited=new[rows,cols]//give it the same length and width as fabric
+       for(int i=0;i<visited.length;i++)//
        {
-       if(row[i]!>130||col[i]!<130)
+        for(int j=0;j<visited[i].length;j++)
        {
+        if(visited[i][j]!=0)
+       {
+         System.out.println("Value is"+ visited[i][j]);
+       if(visited[i][j]<=100)
+       {
+       if(visited[i][j]>=131)
+         System.out.println("Value is"+ visited[i][j]);
+       {
+        System.out.println("Nope");
+        System.out.println("Value is"+ visited[i][j]);
        return false;
        }
        }
        }
+      }
        }
-       }
+        System.out.println("Yep");
        return true;
      
        
        }
+       
 
 	}
 
